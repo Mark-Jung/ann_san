e25163a25be8c2dfc55bb5057658706edefe5baa
Mark Jung <gujung2022@u.northwestern.edu>
2018-11-17 21:13:47 -0600

    initial commit with readme

---
+++ data_aggr/main.py
@@ -0,0 +1,28 @@
+"""
+    1. average the popularities of the top kpop popularities, 3 max
+        a. 
+    2. read all the songs of the playlist from the playlist.txt 10~
+    3. store a hashset of song ids 
+    4. With 3, get audio features and track information 2
+    5. turn the return object of 3 into rows of train.csv 
+"""
+
+"""
+    Spotify Class:
+        1. 
+        Get info of tracks
+            input: list of spotify id, field wanted
+            output: dictionary of song ids: field wanted
+        2.
+        Get spotify id of songs of a playlist
+            input: playlist link
+            output: list of song ids
+        3.
+        Get audio features of tracks
+            input: list of spotify id
+            output: dictionary of song ids: dictionary of audio data
+"""
+
+
+
+
